eafab4127e645fe5ba764ab72fd371e21de566b9
kmenghini <kaitlyn.menghini@gmail.com>
2017-12-21 12:17:27 -0800

    able to download html from websites

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var htmlFetcher = require('../workers/htmlfetcher.js')
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -56,16 +57,18 @@ exports.isUrlArchived = function(url, callback) {
 };
 
 exports.downloadUrls = function(urls) {
-  
   for (var i = 0; i < urls.length; i++) {
     var url = urls[i];
     exports.isUrlArchived(url, function(exists) {
-      if (!exists) {    
-        fs.writeFile(exports.paths.archivedSites + '/' + url, 'use html fetcher', 'utf8', function(err) {
-          if (err) {
-            throw err;
-          }
-        });
+      if (!exists) {  
+        htmlFetcher.fetchHtml(url, function(content) {
+          console.log('content....', content);
+          fs.writeFile(exports.paths.archivedSites + '/' + url, content, 'utf8', function(err) {
+            if (err) {
+              throw err;
+            }
+          });
+        });  
       }
     });
   }
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,23 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var archive = require('../helpers/archive-helpers');
+var http = require('http');
+
+exports.fetchHtml = function (url, callback) {
+  var options = {
+    host: url,
+    port: 80,
+    path: '/'
+  };
+  var content = '';
+  var req = http.request(options, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function(chunk) {
+      content += chunk;
+    });
+    res.on('end', function() {
+      callback(content);
+    });
+  });
+  req.end();
+};
