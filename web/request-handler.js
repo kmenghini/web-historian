ded0c5b838a3da932c2daa52b782a7acb0c06d7c
kmenghini <kaitlyn.menghini@gmail.com>
2017-12-20 20:04:53 -0800

    got archived websites to load

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,75 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var http = require('./http-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var {headers, method, url} = req;
+  var filePath = '.' + url;
+  var headers = http.headers;
+
+  // fs.readFile(archive.paths.siteAssets + '/index.html', 'utf8', function(err, data) {
+  //   if (err) {
+  //     console.log('ERROR!');
+  //     throw err;
+  //   }   
+  //   res.writeHead(200, headers);
+  //   res.write(data);
+  //   res.end();
+  // });
+
+  // http.serveAssets(res, archive.paths.siteAssets + '/index.html', function() {
+  //   console.log('done serving asset!');
+  // });
+ 
+  if (method === 'GET') {
+    archive.isUrlInList(filePath, function(exists) {
+      if (exists) {
+        archive.isUrlArchived(filePath, function(exists) {
+          if (exists) {
+            fs.readFile(archive.paths.archivedSites + filePath.slice(1), 'utf8', function(err, data) {
+              console.log('file path...', archive.paths.archivedSites + filePath.slice(1));
+              if (err) {
+                console.log('ERROR!');
+                throw err;
+              }   
+              res.writeHead(200, headers);
+              res.write(data);
+              res.end();
+            });
+          } else {
+            fs.readFile(archive.paths.siteAssets + '/loading.html', 'utf8', function(err, data) {
+              if (err) {
+                console.log('ERROR!');
+                throw err;
+              }   
+              res.writeHead(200, headers);
+              res.write(data);
+              res.end();
+            });
+          }
+        });
+      } else {
+        archive.addUrlToList(filePath, function() {
+          fs.readFile(archive.paths.siteAssets + '/loading.html', 'utf8', function(err, data) {
+            if (err) {
+              console.log('ERROR!');
+              throw err;
+            }   
+            res.writeHead(200, headers);
+            res.write(data);
+            res.end();
+          });
+        });
+      }
+    });
+  }
+
+  
+
+
+
+
 };
